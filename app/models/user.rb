db460541575d18565ddf082824a4ae5457f8cb9f
penpen <penpen8103@gmail.com>
2019-09-10 21:01:38 +0900

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -25,7 +25,6 @@ class User < ApplicationRecord
     update_attribute(:remember_digest,User.digest(remember_token))
   end
 
-    # トークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
      digest = send("#{attribute}_digest")
      return false if digest.nil?
@@ -37,12 +36,26 @@ class User < ApplicationRecord
   end
 
   def activate
-   update_attribute(:activated,    true)
-   update_attribute(:activated_at, Time.zone.now)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   def send_activation_email
-   UserMailer.account_activation(self).deliver_now
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
@@ -51,7 +64,6 @@ class User < ApplicationRecord
     self.email = email.downcase
   end
 
-  # 有効化トークンとダイジェストを作成および代入する
   def create_activation_digest
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
